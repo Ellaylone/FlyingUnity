b4901505f29f0b7c27e91042596a7443e68e5ae8
Ellaylone <heamik91@yandex.ru>
2014-12-03 22:35:43 +0000

    start shooting

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootin : MonoBehaviour {
+
+	float cooldownTime = 0;
+	public float fireDelay = 0.25f;
+
+	// Update is called once per frame
+	void Update () {
+		cooldownTime -= Time.deltaTime;
+		if (Input.GetButton ("Fire1")) {
+			//Shoot!
+			cooldownTime = fireDelay;
+		}
+	}
+}
