3e07ee3883cdf4c8ea7ff24d695a00ff56abeb40
Ellaylone <heamik91@yandex.ru>
2014-12-04 22:12:01 +0000

    player shooting

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerShootin : MonoBehaviour {
+public class PlayerShooting : MonoBehaviour {
 
-	float cooldownTime = 0;
+	public GameObject bulletPrefab;
+
+	public Vector3 bulletOffset = new Vector3 (0, 0.5f, 0);
+
+	float cooldownTimer = 0;
 	public float fireDelay = 0.25f;
 
 	// Update is called once per frame
 	void Update () {
-		cooldownTime -= Time.deltaTime;
-		if (Input.GetButton ("Fire1")) {
+		cooldownTimer -= Time.deltaTime;
+		if (Input.GetButton ("Fire1") && cooldownTimer <= 0) {
 			//Shoot!
-			cooldownTime = fireDelay;
+			cooldownTimer = fireDelay;
+
+			Vector3 offset = transform.rotation * bulletOffset;
+
+			Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
 		}
 	}
 }
